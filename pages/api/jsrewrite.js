41c65c15c19a5dff8842414c4a1f83c55d4c3cd8
Zuolan <i@zuolan.me>
2022-05-17 22:06:04 +0800

    fix: Adapted to Craft new style.

--- pages/api/jsrewrite.js
+++ pages/api/jsrewrite.js
@@ -18,8 +18,8 @@ module.exports = async (req, res) => {
       '("img",{className:e.className,alt:"logo",src:"/favicon.svg"})'
     ) // replace loading logo to favicon.svg
 
-    // don't show the "/b/*" and "/x/*" in the url
-    // .replace('window.history.pushState({lukiNavIndex:c.length-1},w.breadcrumb,e.deepLinkUrl)', '""')
+  // don't show the "/b/*" and "/x/*" in the url
+  // .replace('window.history.pushState({lukiNavIndex:c.length-1},w.breadcrumb,e.deepLinkUrl)', '""')
   res.setHeader('Content-Type', 'text/javascript; charset=utf-8')
   res.send(modifyResText)
 }
