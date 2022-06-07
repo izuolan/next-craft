654743644a9eb642474697df23969481c96cd882
Zuolan <i@zuolan.me>
2022-06-08 00:30:38 +0800

    fix: ImageFallBack alt text.

--- components/NotePost.js
+++ components/NotePost.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
-// import Image from 'next/image'
-import ImageFallback from './ImageFallBack'
+import ImageFallback from './ImageFallBack.js'
 import { lang } from '@/lib/lang'
 import { useRouter } from 'next/router'
 
