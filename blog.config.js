9324c37946a7db8195a2a3c93ab5fbe4252e1332
KaveeGimX <95114086+KaveeshaGimsara@users.noreply.github.com>
2023-06-30 21:47:59 +0530

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   craftConfigShareUrl: process.env.CRAFT_CONFIG_SHARE_URL
     ? process.env.CRAFT_CONFIG_SHARE_URL
-    : 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
+    : 'https://www.craft.me/s/FGpCgXtii64gOG', // The link to share your craft config
   seo: {
     keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
   },
